8101f878ed4e9f9322c7eb0d432eb7c12cff73c5
Luis Manuel Miralles Martinez <luis.manuel.miralles.martinez@gmail.es>
2021-11-15 12:36:06 +0100

    autocomplete finished added

--- src/app/auth/components/register-page/register-page.component.ts
+++ src/app/auth/components/register-page/register-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { PaddleLevelApiResponse } from '../../interfaces/paddle-level-api-response';
+import { AuthApiService } from '../../services/auth-api.service';
 
 @Component({
   selector: 'app-register-page',
@@ -16,14 +19,36 @@ export class RegisterPageComponent implements OnInit {
     passwordConfirmation: [''],
   });
 
-  constructor(private fb: FormBuilder) { }
+  public paddleLevels: PaddleLevelApiResponse[] = [];
+  filteredPaddleLevels!: Observable<PaddleLevelApiResponse[]>;
+
+  constructor(private fb: FormBuilder, private authApiService: AuthApiService) { }
 
   ngOnInit(): void {
+    this.authApiService.getPaddleLevels().subscribe(res => {
+      this.paddleLevels = res;
+      this.registerForm.controls['paddleLevel'].setValue(''); //mete un valor vacío y activa el valueChanges y así se cargan los valores
+    });
+    
+    this.filteredPaddleLevels = this.registerForm.controls['paddleLevel'].valueChanges.pipe(
+      map((value: string | PaddleLevelApiResponse) => {
+        if(typeof value != "string") value = value.name;
+        return this._filter(value);
+      }),
+    );
   }
 
+
+  displayFn(paddleLevel: PaddleLevelApiResponse): string {
+    return paddleLevel && paddleLevel.name ? paddleLevel.name : '';
+  }
+
+  private _filter(value: string): PaddleLevelApiResponse[] {
+    return this.paddleLevels.filter(pl => pl.name.toLowerCase().includes(value.toLowerCase()));
+  }
+  
   onRegisterSubmit(){
-    console.log("Register form submitted ", this.registerForm.value);
-    
+    console.log("Register form submitted ", this.registerForm.value);    
   }
 
 }
