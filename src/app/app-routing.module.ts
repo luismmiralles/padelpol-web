9aa369b82703df32388d6afbf314b59629989333
Luis Manuel Miralles Martinez <luis.manuel.miralles.martinez@gmail.es>
2021-11-16 12:05:17 +0100

    games module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }];
+const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+{ path: 'partidos', loadChildren: () => import('./games/games.module').then(m => m.GamesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
